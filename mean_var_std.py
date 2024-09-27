7fd72367c98dfbf2dce7d2540547df044fa798a6
milenacasttro <milena.castro@sou.inteli.edu.br>
2024-09-27 15:20:29 -0300

    :sparkles: fix: adiciona a função

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,19 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
+    
+    matriz = np.array(list).reshape(3,3)
 
-
-
-
+    calculations = {
+        'mean': [np.mean(matriz, axis=0).tolist(), np.mean(matriz, axis=1).tolist(), np.mean(matriz).item()],
+        'variance': [np.var(matriz, axis=0).tolist(), np.var(matriz, axis=1).tolist(), np.var(matriz).item()],
+        'standard deviation': [np.std(matriz, axis=0).tolist(), np.std(matriz, axis=1).tolist(), np.std(matriz).item()],
+        'max': [np.max(matriz, axis=0).tolist(), np.max(matriz, axis=1).tolist(), np.max(matriz).item()],
+        'min': [np.min(matriz, axis=0).tolist(), np.min(matriz, axis=1).tolist(), np.min(matriz).item()],
+        'sum': [np.sum(matriz, axis=0).tolist(), np.sum(matriz, axis=1).tolist(), np.sum(matriz).item()]
+    }
+    
     return calculations
+    
